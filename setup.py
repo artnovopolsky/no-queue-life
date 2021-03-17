3f235c49d69634e1b9c750ea83afdb8f69936462
Slava <slavkostrov@gmail.com>
2021-03-17 15:34:42 +0300

    [ADD] Added setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+"""
+setuptools.setup(
+    name="no_queue_life",
+    version="1.0.0",
+    author="",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/artnovopolsky/no-queue-life",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires='>=3.8'
+)
+"""
+
