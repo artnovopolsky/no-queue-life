3d9a74b59fb2aa9b1cfba17befae641e2081d571
Slava <slavkostrov@gmail.com>
2021-03-20 22:19:11 +0300

    [UPDATE] Updated setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-"""
+
 setuptools.setup(
     name="no_queue_life",
     version="1.0.0",
-    author="",
-    author_email="",
-    description="",
+    author="Artyom Novopolsky, Pavel Mescheryakov, Vyacheslav Kostrov",
+    author_email="slavkostrov@gmail.com",
+    description="no-queue-life project",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/artnovopolsky/no-queue-life",
@@ -22,8 +22,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[],
+    install_requires=["django==3.1.7"],
     python_requires='>=3.8'
 )
-"""
 
