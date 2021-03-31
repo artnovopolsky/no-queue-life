57f9e8b20176fe7156c1c0b4d2f9fa5a38eb6158
artnovopolsky <artemnvp@rambler.ru>
2021-03-31 23:07:50 +0300

    [UPDATE] Updated requirements.txt and some config files

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@
 import os
 import sys
 import django
-sys.path.insert(0, os.path.abspath('.'))
+
+
+sys.path.insert(0, os.path.abspath('..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'no_queue_life.settings'
 django.setup()
 
